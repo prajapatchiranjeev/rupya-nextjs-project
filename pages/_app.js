7c012632bb257122fa8ab952328d0486d5625488
prajapatchiranjeev <chiranjeevprajapat089@gmail.com>
2025-08-30 14:36:08 +0530

    commit changes

--- pages/_app.js
+++ pages/_app.js
@@ -7,9 +7,7 @@ export default function MyApp({ Component, pageProps }) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
-        <!-- Tailwind CDN (for prototype) -->
         <script src="https://cdn.tailwindcss.com"></script>
-        <!-- Font Awesome -->
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet" />
         <title>Rupya Digital</title>
       </Head>
