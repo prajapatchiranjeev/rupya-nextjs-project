c5a91ff1fbc48fee38b8676dd80162382609e8da
CHIRANJEEV PRAJAPAT <73808605+prajapatchiranjeev@users.noreply.github.com>
2025-09-01 17:48:16 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,24 @@
-import Head from 'next/head';
-import '../styles.css';
+import Script from "next/script";
+import Head from "next/head";
 
-export default function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+          rel="stylesheet"
+        />
+      </Head>
 
-  {/* Tailwind CDN (for prototype) */}
-  <script src="https://cdn.tailwindcss.com"></script>
+      {/* Tailwind via CDN (not recommended for production, better to install via npm) */}
+      <Script src="https://cdn.tailwindcss.com" strategy="beforeInteractive" />
 
-  {/* Font Awesome */}
-  <link
-    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
-    rel="stylesheet"
-  />
-</Head>
+      <Component {...pageProps} />
     </>
-  )
+  );
 }
+
+export default MyApp;
