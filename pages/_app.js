af2f31dd58e13e82c32ec95d4c24beb277ae954f
CHIRANJEEV PRAJAPAT <73808605+prajapatchiranjeev@users.noreply.github.com>
2025-09-01 17:46:04 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,13 +5,18 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-        <script src="https://cdn.tailwindcss.com"></script>
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet" />
-        <title>Rupya Digital</title>
-      </Head>
-      <Component {...pageProps} />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="icon" href="/favicon.ico" />
+
+  {/* Tailwind CDN (for prototype) */}
+  <script src="https://cdn.tailwindcss.com"></script>
+
+  {/* Font Awesome */}
+  <link
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+    rel="stylesheet"
+  />
+</Head>
     </>
   )
 }
